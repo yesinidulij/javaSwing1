6cfc6f1aa9c398f18b3e9b2bc7cbdbdc6187e526
yesinidulij <yesinidulij123@gmail.com>
2021-12-19 11:10:03 -0800

    ffd

---
+++ src/com/windowsfreeapps/Main.java
@@ -0,0 +1,9 @@
+package com.windowsfreeapps;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("trying to github");
+    }
+}
